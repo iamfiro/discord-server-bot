3260340155d930b4c30d3934714a6775651e1188
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-06 01:15:30 +0900

    feat: Refactor kick command to use handleSanctionCommandRequest function

--- src/context/kick.ts
+++ src/context/kick.ts
@@ -1,21 +1,8 @@
 import { ActionRowBuilder, ApplicationCommandType, ChatInputCommandInteraction, Colors, ContextMenuCommandBuilder, ContextMenuCommandInteraction, EmbedBuilder, ModalBuilder, ModalSubmitInteraction, TextChannel, TextInputBuilder, TextInputStyle } from "discord.js";
+import handleSanctionCommandRequest from "../handler/sanctions";
 
 async function handler(target: string, interaction: ContextMenuCommandInteraction) {
-    const modal = new ModalBuilder()
-        .setCustomId(`kick-${process.env.KICK_LOG_CHANNEL}-${target}`)
-        .setTitle("서버 추방")
-    
-    const reason = new TextInputBuilder()
-        .setCustomId('input-reason')
-        .setLabel("사유")
-        .setPlaceholder("사유를 입력해주세요.")
-        .setStyle(TextInputStyle.Short);
-    
-    const ActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(reason);
-
-    modal.addComponents(ActionRow);
-
-    await interaction.showModal(modal)
+    handleSanctionCommandRequest({ interaction, target, type: 'kick', channelId: process.env.KICK_LOG_CHANNEL || '', title: '서버 추방' })
 }
 
 export default {
