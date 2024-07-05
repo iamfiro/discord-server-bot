fde8192ef93977507cc5c1eacd5fb1d3ab92cb3b
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-06 00:48:16 +0900

    feat: Update Discord bot to register slash commands and context menu commands

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Client, IntentsBitField, Routes } from 'discord.js';
+import { ChatInputCommandInteraction, Client, ContextMenuCommandInteraction, IntentsBitField, ModalSubmitInteraction, Routes } from 'discord.js';
 import Logger from "./lib/logger";
 import { client, rest } from './lib/bot';
 import ping from './commands/ping';
@@ -9,64 +9,120 @@ import decreaseWarn from './context/decreaseWarn';
 import increaseWarn from './context/increaseWarn';
 import handleSanctions from './event/sanctions';
 import deleteMessage from './commands/manage/deleteMessage';
+import { ModalHandlerListType } from './types/interactionEvent';
 
+// Logger instance 생성
 const logger = new Logger();
 
-client.on('ready', async () => {
-    if(!client.user) return;
+/**
+ * Register all commands and context menus with Discord API
+ */
+async function registerCommands() {
+    if (!client.user) return;
 
-    logger.info(`Register commands...`);
+    logger.info(`Registering commands...`);
 
     const botID = process.env.BOT_ID;
-    if (!botID) return logger.error('BOT_TOKEN is not defined.');
+    if (!botID) {
+        return logger.error('BOT_ID is not defined.');
+    }
 
-    await rest.put(Routes.applicationCommands(botID), {
-        body: [
-            // Slash Command
-            ping.info.toJSON(),
-            deleteMessage.info.toJSON(),
-            // Context Menu
-            kick.info.toJSON(),
-            ban.info.toJSON(),
-            decreaseWarn.info.toJSON(),
-            increaseWarn.info.toJSON()
-        ]
-    });
+    try {
+        await rest.put(Routes.applicationCommands(botID), {
+            body: [
+                // Slash Command
+                ping.info.toJSON(),
+                deleteMessage.info.toJSON(),
+                // Context Menu
+                kick.info.toJSON(),
+                ban.info.toJSON(),
+                decreaseWarn.info.toJSON(),
+                increaseWarn.info.toJSON()
+            ]
+        });
 
-    logger.info(`Commands were registered successfully!`);
+        logger.info(`Commands were registered successfully!`);
+    } catch (error) {
+        logger.error(`Failed to register commands: ${error}`);
+    }
+
     logger.info(`Logged in as ${client.user.tag}!`);
+}
+
+client.on('ready', registerCommands);
+
+/**
+ * interactionCreate 이벤트 핸들러
+ */
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        handleChatInputCommand(interaction);
+    } else if (interaction.isContextMenuCommand()) {
+        handleContextMenuCommand(interaction);
+    } else if (interaction.isModalSubmit()) {
+        handleModalSubmit(interaction);
+    }
 });
 
-client.on('interactionCreate', async interaction => {
-    if(interaction.isChatInputCommand()) {
-        switch (interaction.commandName) {
-            case 'ping': ping.handler(interaction); break;
-            case '청소': deleteMessage.handle(interaction); break;
+/**
+ * 채팅 입력 명령어 처리 함수
+ * @param {Interaction} interaction - Discord 상호작용 객체
+ */
+const handleChatInputCommand = (interaction: ChatInputCommandInteraction) => {
+    switch (interaction.commandName) {
+        case 'ping':
+            ping.handler(interaction);
+            break;
+        case '청소':
+            deleteMessage.handle(interaction);
+            break;
+    }
+};
+
+/**
+ * 컨텍스트 메뉴 명령어 처리 함수
+ * @param {Interaction} interaction - Discord 상호작용 객체
+ */
+const handleContextMenuCommand = (interaction: ContextMenuCommandInteraction) => {
+    switch (interaction.commandName) {
+        case '서버 추방':
+            kick.handler(interaction.targetId, interaction);
+            break;
+        case '서버 차단':
+            ban.handler(interaction.targetId, interaction);
+            break;
+        case '경고 추가':
+            increaseWarn.handler(interaction.targetId, interaction);
+            break;
+        case '경고 차감':
+            decreaseWarn.handler(interaction.targetId, interaction);
+            break;
+    }
+};
+
+/**
+ * 모달 제출 처리 함수
+ * @param {Interaction} interaction - Discord 상호작용 객체
+ */
+const handleModalSubmit = (interaction: ModalSubmitInteraction) => {
+    const handlers: ModalHandlerListType = {
+        kick: handleSanctions,
+        ban: handleSanctions,
+        increaseWarn: handleSanctions,
+        decreaseWarn: handleSanctions,
+    };
+
+    for (const key in handlers) {
+        if (interaction.customId.startsWith(key)) {
+            handlers[key](interaction);
         }
     }
-
-    if(interaction.isContextMenuCommand()) {
-        switch (interaction.commandName) {
-            case '서버 추방': kick.handler(interaction.targetId, interaction); break;
-            case '서버 차단': ban.handler(interaction.targetId, interaction); break;
-            case '경고 추가': increaseWarn.handler(interaction.targetId, interaction); break;
-            case '경고 차감': decreaseWarn.handler(interaction.targetId, interaction); break;
-        }
-    }
-
-    if(interaction.isModalSubmit()) {
-        if(interaction.customId.startsWith('kick')) handleSanctions(interaction);
-        if(interaction.customId.startsWith('ban')) handleSanctions(interaction);
-        if(interaction.customId.startsWith('increaseWarn')) handleSanctions(interaction);
-        if(interaction.customId.startsWith('decreaseWarn')) handleSanctions(interaction);
-    }
-})
-
+};
 
 ;(async () => {
     try {
         await client.login(process.env.BOT_TOKEN);
     } catch (error) {
-        logger.error(error);
+        logger.error(`Login failed: ${error}`);
     }
 })();
