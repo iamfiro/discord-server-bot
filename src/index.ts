1ce3151adef6b3b3e433730ff6867cecc806b740
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-05 21:10:26 +0900

    chore: Update logging in index.ts to use Logger class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import 'dotenv/config';
 import { Client } from 'discord.js';
 
+import Logger from "./lib/logger";
+
+const logger = new Logger();
+
 const client = new Client({
     intents: ['GuildBans', 'Guilds', "GuildMessages", 'GuildMembers', 'MessageContent']
 });
 
 client.on('ready', () => {
     if(!client.user) return console.log('No user');
-    console.log(`Logged in as ${client.user.tag}!`);
+    logger.info(`Logged in as ${client.user.tag}!`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
