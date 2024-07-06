bad50ae04c160853d337e421b44f0a57a5df0fbd
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-06 17:29:20 +0900

    refactor: Update Embed module to include welcome message

--- src/lib/common/embed.ts
+++ src/lib/common/embed.ts
@@ -6,3 +6,20 @@ export const EmbedError = (message: string): EmbedBuilder => {
         .setDescription(message)
         .setColor(Colors.Red);
 }
+
+export const EmbedCommandSuccess = (): EmbedBuilder => {
+    return new EmbedBuilder()
+        .setTitle("✅ 명령어 실행 성공")
+        .setDescription("명령어가 성공적으로 실행됨")
+        .setColor(Colors.Green);
+}
+
+export const EmbedWelcome = (): EmbedBuilder => {
+    return new EmbedBuilder()
+        .setTitle("🎉 환영합니다!")
+        .setDescription(`
+            이 입장권은 서버에서 1달동안 활동이 없으면 만료돼요.
+            대충 ㅁㄴㅇㄹ
+        `)
+        .setColor(Colors.Green);
+}
