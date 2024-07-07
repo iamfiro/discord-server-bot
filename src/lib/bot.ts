fb22127ff7d73199f447c0e2fe5913881415a0c6
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-07 18:09:31 +0900

    refactor: Update Discord.js import to include partials for handling log deleted message

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, REST } from 'discord.js';
+import { Client, GatewayIntentBits, Partials, REST } from 'discord.js';
 import 'dotenv/config';
 
 export const client = new Client({
@@ -8,6 +8,13 @@ export const client = new Client({
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
+    ],
+    partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.User,
+        Partials.Reaction
     ]
 });
 
